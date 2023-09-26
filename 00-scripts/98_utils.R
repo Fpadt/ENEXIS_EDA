a2b214fb7eb57cef7c1129a18e80270fbce5cab3
Floris Padt <floris@fmpadt.nl>
2023-09-26 10:30:52 +0000

    delete useless file

--- 00-scripts/98_utils.R
+++
@@ -1,8 +0,0 @@
-
-read_lines(file = "~/data/00-raw/prd/ch1/PRODUCTMD.csv", skip = 42224, n_max = 10)
-
-read_lines(file = "~/data/00-raw/prd/tr1/PRODUCTMD.csv", skip = 13284, n_max = 10)
-
-read_lines(file = "~/data/00-raw/prd/pe1/PRODUCTMD.csv", skip = 47349, n_max = 10)
-
-
